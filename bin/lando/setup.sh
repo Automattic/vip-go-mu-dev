b96bc6d33f1b9ae8ac2f00ba3a4297df28b1b979
Mohammad Jangda <batmoo@gmail.com>
2020-03-01 14:42:40 -0500

    Copy over WP settings from production
    And add them directly to the wp-config file.
    
    - Load vip-config (if present)
    - Load error handler
    - WP_DEBUG + WP_DEBUG_LOG
    - No file system edits from WP (containers are not read-only)
    - Max memory limit

--- bin/lando/setup.sh
+++ bin/lando/setup.sh
@@ -33,15 +33,28 @@ wp config create \
 	--dbname=$DB_NAME \
 	--dbuser=$DB_USER \
 	--dbpass=$DB_PASS \
-	--dbhost=$DB_HOST
+	--dbhost=$DB_HOST \
+	--extra-php <<PHP
+// Read-only filesystem
+define( 'DISALLOW_FILE_EDIT', true );
+define( 'DISALLOW_FILE_MODS', true );
+define( 'AUTOMATIC_UPDATER_DISABLED', true );
 
-echo "Adding wp-config constants"
+// Server limits
+define( 'WP_MAX_MEMORY_LIMIT', '512M' );
 
-wp config set \
-	--path=$LANDO_WEBROOT \
-	--type=constant \
-	--raw \
-	WP_DEBUG true
+// Load custom error logging functions, if available
+if ( file_exists( ABSPATH . '/wp-content/mu-plugins/lib/wpcom-error-handler/wpcom-error-handler.php' ) ) {
+	require_once ABSPATH . '/wp-content/mu-plugins/lib/wpcom-error-handler/wpcom-error-handler.php';
+}
+
+if ( file_exists( __DIR__ . '/wp-content/vip-config/vip-config.php' ) ) {
+	require_once( __DIR__ . '/wp-content/vip-config/vip-config.php' );
+}
+
+define( 'WP_DEBUG', true );
+define( 'WP_DEBUG_LOG', true );
+PHP
 
 # Install WordPress
 echo_heading "Installing WordPress $WP_VERSION"
