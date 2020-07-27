a9ac709f0723661037c9234a805fcb533c137d70
Brandon Skinner <brandon.skinner@automattic.com>
2020-07-27 17:06:41 -0600

    Add repo and branch switching (#25)
    * Add repo and branch switching
    
    * Added remaining defined constants to sed expr
    
    * Added --rebuild flag to vip-init.sh (#23)
    
    * Added --rebuild flag to vip-init.sh
    
    * Added destroy flag. Changed rebuild flag.
    
    * Add Memcached service and php-memcache extension
    
    * Force yes on pecl. Use same install for all
    
    * Update TODO list (#28)
    
    * Set WP_ENVIRONMENT_TYPE
    
    https://make.wordpress.org/core/2020/07/24/new-wp_get_environment_type-function-in-wordpress-5-5/
    
    * Update bin/lando/vip-switch.sh
    
    Co-authored-by: Mohammad Jangda <mo@automattic.com>
    
    * Switch from cp to rsync for the copying
    
    * Remove --single-branch option since entire git repo is copied
    
    * Remove extraneous var. Added clarifying comment
    
    * Add repo and branch switching
    
    * Added remaining defined constants to sed expr
    
    * Update bin/lando/vip-switch.sh
    
    Co-authored-by: Mohammad Jangda <mo@automattic.com>
    
    * Switch from cp to rsync for the copying
    
    * Remove --single-branch option since entire git repo is copied
    
    * Remove extraneous var. Added clarifying comment
    
    * Add defined checks. Remove file edits
    
    Co-authored-by: Rinat Khaziev <rinat.khaziev@gmail.com>
    Co-authored-by: Rinat K <rinat@automattic.com>
    Co-authored-by: Mohammad Jangda <mo@automattic.com>

--- bin/lando/setup.sh
+++ bin/lando/setup.sh
@@ -42,16 +42,34 @@ wp config create \
 	--dbpass=$DB_PASS \
 	--dbhost=$DB_HOST \
 	--extra-php <<PHP
+// VIP Config
+// Execute first so that we can handle constant conflicts easily
+if ( file_exists( __DIR__ . '/wp-content/vip-config/vip-config.php' ) ) {
+	require_once( __DIR__ . '/wp-content/vip-config/vip-config.php' );
+}
+
 // Env constants
-define( 'FILES_CLIENT_SITE_ID', 200508 );
+if ( ! defined( 'FILES_CLIENT_SITE_ID' ) ) {
+	define( 'FILES_CLIENT_SITE_ID', 200508 );
+}
 
 // Read-only filesystem
-define( 'DISALLOW_FILE_EDIT', true );
-define( 'DISALLOW_FILE_MODS', true );
-define( 'AUTOMATIC_UPDATER_DISABLED', true );
+if ( ! defined( 'DISALLOW_FILE_EDIT' ) ) {
+	define( 'DISALLOW_FILE_EDIT', true );
+}
+
+if ( ! defined( 'DISALLOW_FILE_MODS' ) ) {
+	define( 'DISALLOW_FILE_MODS', true );
+}
+
+if ( ! defined( 'AUTOMATIC_UPDATER_DISABLED' ) ) {
+	define( 'AUTOMATIC_UPDATER_DISABLED', true );
+}
 
 // Server limits
-define( 'WP_MAX_MEMORY_LIMIT', '512M' );
+if ( ! defined( 'WP_MAX_MEMORY_LIMIT' ) ) {
+	define( 'WP_MAX_MEMORY_LIMIT', '512M' );
+}
 
 // Load custom error logging functions, if available
 if ( file_exists( ABSPATH . '/wp-content/mu-plugins/lib/wpcom-error-handler/wpcom-error-handler.php' ) ) {
@@ -59,28 +77,44 @@ if ( file_exists( ABSPATH . '/wp-content/mu-plugins/lib/wpcom-error-handler/wpco
 }
 
 // VIP Search
-define( 'USE_VIP_ELASTICSEARCH', true );
-define( 'VIP_ENABLE_ELASTICSEARCH_QUERY_INTEGRATION', true );
-define( 'VIP_ELASTICSEARCH_ENDPOINTS', [
-	'http://vip-search:9200',
-] );
-define( 'VIP_ELASTICSEARCH_USERNAME', 'test_user' );
-define( 'VIP_ELASTICSEARCH_PASSWORD', 'test_password' );
-
-// Cron Control
-define( 'WPCOM_VIP_LOAD_CRON_CONTROL_LOCALLY', true );
-define( 'WP_CRON_CONTROL_SECRET', 'this-is-a-secret' );
-
-// VIP Config
-if ( file_exists( __DIR__ . '/wp-content/vip-config/vip-config.php' ) ) {
-	require_once( __DIR__ . '/wp-content/vip-config/vip-config.php' );
+if ( ! defined( 'USE_VIP_ELASTICSEARCH' ) ) {
+	define( 'USE_VIP_ELASTICSEARCH', true );
 }
 
-// This is a development environment
-define( 'WP_ENVIRONMENT_TYPE', 'development' );
+if ( ! defined( 'VIP_ENABLE_ELASTICSEARCH_QUERY_INTEGRATION' ) ) {
+	define( 'VIP_ENABLE_ELASTICSEARCH_QUERY_INTEGRATION', true );
+}
 
-define( 'WP_DEBUG', true );
-define( 'WP_DEBUG_LOG', true );
+if ( ! defined( 'VIP_ELASTICSEARCH_ENDPOINTS' ) ) {
+	define( 'VIP_ELASTICSEARCH_ENDPOINTS', [
+		'http://vip-search:9200',
+	] );
+}
+
+if ( ! defined( 'VIP_ELASTICSEARCH_USERNAME' ) ) {
+	define( 'VIP_ELASTICSEARCH_USERNAME', 'test_user' );
+}
+
+if ( ! defined( 'VIP_ELASTICSEARCH_PASSWORD' ) ) {
+	define( 'VIP_ELASTICSEARCH_PASSWORD', 'test_password' );
+}
+
+// Cron Control
+if ( ! defined( 'WPCOM_VIP_LOAD_CRON_CONTROL_LOCALLY' ) ) {
+	define( 'WPCOM_VIP_LOAD_CRON_CONTROL_LOCALLY', true );
+}
+
+if ( ! defined( 'WP_CRON_CONTROL_SECRET' ) ) {
+	define( 'WP_CRON_CONTROL_SECRET', 'this-is-a-secret' );
+}
+
+if ( ! defined( 'WP_DEBUG' ) ) {
+	define( 'WP_DEBUG', true );
+}
+
+if ( ! defined( 'WP_DEBUG_LOG' ) ) {
+	define( 'WP_DEBUG_LOG', true );
+}
 PHP
 
 # Install WordPress
