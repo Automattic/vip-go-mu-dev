7dd577e52c4b1e3c2e5d6f6b5bfd4233241361b1
Suso Bravo-Alvarez <suso@automattic.com>
2020-07-29 11:42:39 +0200

    Use multisite subdomain mode instead of subdirectory mode
    After adding multisite support for subdirectory mode, it was
    easy to notice that it was actually the same as subdomain mode.
    This way we avoid needing to change url options and the domain
    in wp_blogs, and manually created sites will work directly
    without changing their url.

--- bin/lando/add-site.sh
+++ bin/lando/add-site.sh
@@ -29,20 +29,7 @@ wp --path=$LANDO_WEBROOT site list --field=domain | grep -q "^$site_domain$"
 [ $? -eq 0 ] && echo "ERROR: site with domain $site_domain already exists" && exit 1
 
 echo "Creating the new site..."
-site_id=`wp --path=$LANDO_WEBROOT site create --title="$title" --slug="$slug" --porcelain`
-
-# The multisite installation is in subdirectory mode, so we have to change
-# the siteurl and home options to the proper domain
-echo "Changing 'siteurl' and 'home' options to $site_domain..."
-wp --path=$LANDO_WEBROOT --url="http://vip-go-dev.lndo.site/$slug" option update siteurl "http://$site_domain/"
-wp --path=$LANDO_WEBROOT --url="http://vip-go-dev.lndo.site/$slug" option update home "http://$site_domain/"
-
-# We also need to change the domain in wp_blogs. This cannot be done from wp-cli
-# (only from wp-admin), so we need to run a DB query to change it
-echo "Changing the wp_blogs domain to $site_domain..."
-echo "UPDATE wp_blogs SET domain=\"$site_domain\", path=\"/\" WHERE blog_id=$site_id" |
-mysql -u$DB_USER -p$DB_PASS -h$DB_HOST $DB_NAME
-[ $? -ne 0 ] && echo "ERROR: couldn't change the domain in the wp_blogs table" && exit 1
+wp --path=$LANDO_WEBROOT site create --title="$title" --slug="$slug"
 
 echo
 echo "======================================================================"
--- bin/lando/setup-multisite.sh
+++ bin/lando/setup-multisite.sh
@@ -5,13 +5,14 @@ wp core is-installed --network
 [ $? -eq 0 ] && echo "WARNING: Multisite already setup, no need to do anything" && exit 1
 
 echo "Converting installation to multisite..."
-wp core multisite-convert --path=$LANDO_WEBROOT --title="VIP Go Dev Network"
+wp core multisite-convert --path=$LANDO_WEBROOT --title="VIP Go Dev Network" --subdomains
 
-echo "=============================================================="
+echo "=========================================================================="
 echo "WordPress installation converted to multisite"
 echo
-echo "You can now add sites with \"lando add-site <slug> <title>\""
+echo "You can now add sites with:"
+echo "  lando add-site --slug=<slug> --title=\"<title>\""
 echo
-echo "If you add a site manually from wp-admin, please use this as"
-echo "the site url: http://<slug>.vip-go-dev.lndo.site"
-echo "=============================================================="
+echo "You can also add sites manually from wp-admin. In both cases, sites"
+echo "will follow this url schema: http://<slug>.vip-go-dev.lndo.site/"
+echo "=========================================================================="
