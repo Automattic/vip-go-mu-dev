d1d46b9e7a030b71edc469b19a1d685fae6292f8
Suso Bravo-Alvarez <suso@automattic.com>
2020-09-15 20:32:43 +0200

    Make dev setup more robust, detecting missing DB or missing WP installation

--- dockerfiles/dev-tools/setup.sh
+++ dockerfiles/dev-tools/setup.sh
@@ -5,56 +5,64 @@ if [ $# -lt 4 ]; then
   exit 1
 fi
 
-# FIXME: Also check if the DB exists
-if [ -r /wp/config/wp-config.php ]; then
-  echo "Already existing WordPress installation"
-  exit 0
-fi
-
 db_host=$1
 db_admin_user=$2
 wp_url=$3
 wp_title=$4
 multisite_domain=$5
 
-echo "GRANT ALL ON *.* TO 'wordpress'@'localhost' IDENTIFIED BY 'wordpress' WITH GRANT OPTION;" | mysql -h $db_host -u root
-echo "GRANT ALL ON *.* TO 'wordpress'@'%' IDENTIFIED BY 'wordpress' WITH GRANT OPTION;" | mysql -h database -u root
-echo "CREATE DATABASE wordpress;" | mysql -h database -u root
-
-cp /dev-tools/wp-config-defaults.php /wp/config/
-cat /dev-tools/wp-config.php.tpl | sed -e "s/%DB_HOST%/$db_host/" > /wp/config/wp-config.php
-if [ -n "$multisite_domain" ]; then
-  cat /dev-tools/wp-config-multisite.php.tpl | sed -e "s/%DOMAIN%/$multisite_domain/" >> /wp/config/wp-config.php
-fi
-curl -s https://api.wordpress.org/secret-key/1.1/salt/ >> /wp/config/wp-config.php
-
-if [ -n "$multisite_domain" ]; then
-  wp core multisite-install \
-    --path=/wp \
-    --allow-root \
-    --url="$wp_url" \
-    --title="$wp_title" \
-    --admin_user="vipgo" \
-    --admin_email="vip@localhost.local" \
-    --admin_password="password" \
-    --skip-email \
-    --skip-plugins \
-    --subdomains \
-    --skip-config #2>/dev/null
+if [ -r /wp/config/wp-config.php ]; then
+  echo "Already existing wp-config.php file"
 else
-  wp core install \
-    --path=/wp \
-    --allow-root \
-    --url="$wp_url" \
-    --title="$wp_title" \
-    --admin_user="vipgo" \
-    --admin_email="vip@localhost.local" \
-    --admin_password="password" \
-    --skip-email \
-    --skip-plugins #2>/dev/null
+  cp /dev-tools/wp-config-defaults.php /wp/config/
+  cat /dev-tools/wp-config.php.tpl | sed -e "s/%DB_HOST%/$db_host/" > /wp/config/wp-config.php
+  if [ -n "$multisite_domain" ]; then
+    cat /dev-tools/wp-config-multisite.php.tpl | sed -e "s/%DOMAIN%/$multisite_domain/" >> /wp/config/wp-config.php
+  fi
+  curl -s https://api.wordpress.org/secret-key/1.1/salt/ >> /wp/config/wp-config.php
 fi
 
-wp --allow-root elasticpress delete-index
-wp --allow-root elasticpress index --setup
+echo "Checking for database connectivity..."
+echo "SELECT 'testing_db'"  | mysql -h $db_host -u wordpress -pwordpress wordpress
+if [ $? -ne 0 ]; then
+  echo "No WordPress database exists, provisioning..."
+  echo "GRANT ALL ON *.* TO 'wordpress'@'localhost' IDENTIFIED BY 'wordpress' WITH GRANT OPTION;" | mysql -h $db_host -u root
+  echo "GRANT ALL ON *.* TO 'wordpress'@'%' IDENTIFIED BY 'wordpress' WITH GRANT OPTION;" | mysql -h $db_host -u $db_admin_user
+  echo "CREATE DATABASE wordpress;" | mysql -h $db_host -u $db_admin_user
+fi
 
-wp --allow-root user add-cap 1 view_query_monitor
+echo "Checking for WordPress installation..."
+wp --allow-root get option siteurl
+if [ $? -ne 0 ]; then
+  echo "No installation found, installing WordPress..."
+  if [ -n "$multisite_domain" ]; then
+    wp core multisite-install \
+      --path=/wp \
+      --allow-root \
+      --url="$wp_url" \
+      --title="$wp_title" \
+      --admin_user="vipgo" \
+      --admin_email="vip@localhost.local" \
+      --admin_password="password" \
+      --skip-email \
+      --skip-plugins \
+      --subdomains \
+      --skip-config #2>/dev/null
+  else
+    wp core install \
+      --path=/wp \
+      --allow-root \
+      --url="$wp_url" \
+      --title="$wp_title" \
+      --admin_user="vipgo" \
+      --admin_email="vip@localhost.local" \
+      --admin_password="password" \
+      --skip-email \
+      --skip-plugins #2>/dev/null
+  fi
+
+  wp --allow-root elasticpress delete-index
+  wp --allow-root elasticpress index --setup
+
+  wp --allow-root user add-cap 1 view_query_monitor
+fi
